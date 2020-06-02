b85073487dc8d15838281ea087cf8ab780442566
SOBUE Sho <sho@sobue.dev>
2020-06-03 01:23:55 +0900

    Add Javadoc Comments

--- src/main/java/dev/sobue/webflux/WebfluxSampleApplication.java
+++ src/main/java/dev/sobue/webflux/WebfluxSampleApplication.java
@@ -3,11 +3,20 @@ package dev.sobue.webflux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Application Class of Spring Boot.
+ *
+ * @author ssobue
+ */
 @SpringBootApplication
 public class WebfluxSampleApplication {
 
+  /**
+   * Main Method.
+   *
+   * @param args Command Line Arguments.
+   */
   public static void main(String[] args) {
     SpringApplication.run(WebfluxSampleApplication.class, args);
   }
-
 }
--- src/test/java/dev/sobue/webflux/WebfluxSampleApplicationTests.java
+++ src/test/java/dev/sobue/webflux/WebfluxSampleApplicationTests.java
@@ -1,13 +1,27 @@
 package dev.sobue.webflux;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author ssobue
+ * @see WebfluxSampleApplication
+ */
 @SpringBootTest
+@DisplayName("Application Running Test")
 class WebfluxSampleApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+  @Autowired
+  private ApplicationContext context;
 
+  @Test
+  @DisplayName("Test Loading ApplicationContext")
+  void contextLoads() {
+    assertNotNull(context);
+  }
 }
