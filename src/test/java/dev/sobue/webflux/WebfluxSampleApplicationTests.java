d81fe06d27bc9a0a054e9239dacc1754994ff56a
SOBUE Sho <sho@sobue.dev>
2020-06-03 00:37:38 +0900

    Initial commit

---
+++ src/test/java/dev/sobue/webflux/WebfluxSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sobue.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
